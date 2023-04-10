781b2efda5f64ae81ed796d8e6032f0b99f38e3f
MatOliveir <matheus.oliveira@picpay.com>
2023-04-10 16:53:11 -0300

    update doc

--- ide.py
+++ ide.py
@@ -1,20 +1,2 @@
-fin = open('chapter9/words.txt')
-
-def twenty_letters_more():
-    for line in fin:
-        word = line.strip()
-        if len(word) > 20:
-            print(word)
-
-def has_no_e():
-    total = 0
-    no_e = 0
-    for line in fin:
-        word = line.strip()
-        total += 1
-        if 'e' not in word:
-            print(word)
-            no_e +=  1
-    print(no_e/total*100)
-
-has_no_e()
+t = [1, 5, 6 , 3, 4, 2]
+print(t)
